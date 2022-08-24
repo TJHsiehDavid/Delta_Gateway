32e3ebc4428f8b3ada5f5858304ff0ba69c96856
davidhsieh <david.hsieh@deltaww.com>
2022-08-24 10:14:12 +0800

    [PYC] Disable pyc file generate. Since will use .so as encryption file not .pyc.

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 import sys
+# Command below is to Disable pyc file generate.
+sys.dont_write_bytecode = True
 from flask_cors import CORS
 from datetime import datetime,timedelta
 import time
